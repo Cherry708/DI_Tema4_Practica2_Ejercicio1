38336e0f492f822140c5f56d2b261d8d9a771968
Cherry708 <aluser8241@ieselcaminas.org>
2021-11-25 15:15:17 +0100

    Ejercicio 1 finalizado

--- app/src/main/java/com/example/di_tema4_practica2_ejercicio1/ui/books/BooksViewModel.kt
+++ app/src/main/java/com/example/di_tema4_practica2_ejercicio1/ui/books/BooksViewModel.kt
@@ -3,7 +3,9 @@ package com.example.di_tema4_practica2_ejercicio1.ui.books
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-
+/*
+El viewModel se ha autogenerado al usar el Template de bottomAppNavigation
+ */
 class BooksViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
